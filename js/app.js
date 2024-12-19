e8a016c15687a9a619632e613074243f346de36b
Priya <pkesari@cisco.com>
2024-12-19 23:08:36 +0530

    fix(guest-calling): removed tokens

--- js/app.js
+++ js/app.js
@@ -11,9 +11,8 @@ const secondCallTimer = document.querySelector('#secondCallNotification #call-ti
 const profileDropDown = document.getElementById("myDropdown");
 const profileOnline = document.querySelector(".dropbtn #availability");
 
-let service_app_token = 'MzA3MWQ4NGEtNDA5Ni00MDIwLThkOTctOGZlMmI1N2M1ODk5ZTA0NjAyZDYtNTk5_PF84_ef70fae6-b079-45e6-901e-0a1ba9856721'; // 6100 queue
-// let service_app_token = 'YTgyZjkxNTMtM2IzOC00NjYwLTlhMmQtMjk2Y2EzMjUyY2Y2MGFkN2ZkNjAtZTc5_P0A1_b657fcf3-6b06-46a4-ab0e-f23fceb7a447'; //5200 queue
-// const refresh_token = 'ZDQzZDE3MjgtYThmNS00NzkwLThhZGMtYjdmYzMxNjAyOTQwZmM1ZGY4NDgtOTU4_PF84_ef70fae6-b079-45e6-901e-0a1ba9856721';
+let service_app_token = ''; // Add the service app account token here
+// const refresh_token = '';
 
 class callNotificationElement {
     constructor(element,callTimerElement){
@@ -107,8 +106,8 @@ function openCallNotification(callObj) {
   
 //   const urlencoded = new URLSearchParams();
 //   urlencoded.append("grant_type", "refresh_token");
-//   urlencoded.append("client_id", "C47467d033f61aee5270717f87c571d0989fd9a89d20ebc8180c175daf58882a0");
-//   urlencoded.append("client_secret", "db1bcc29eec3503abb619050ec038145fc656d2aeb4ede6fcad8eccbda06c159");
+//   urlencoded.append("client_id", "");
+//   urlencoded.append("client_secret", "");
 //   urlencoded.append("refresh_token", refresh_token);
 
 //   const request = {
@@ -180,7 +179,7 @@ async function getWebexConfig(userType) {
   // Below values are hardcoded tokens for licensed users
   // let access_token;
   // if (userType === 'customer') {
-  //   access_token = "eyJhbGciOiJSUzI1NiJ9.eyJjbHVzdGVyIjoiUDBBMSIsInByaXZhdGUiOiJleUpqZEhraU9pSktWMVFpTENKbGJtTWlPaUpCTVRJNFEwSkRMVWhUTWpVMklpd2lZV3huSWpvaVpHbHlJbjAuLmp1c09mVTI5R18xR3FMWXYxSnAtTHcuUVhadFZMTkJGeHJGV09BcDBCX2FNT3EwdTBWWjZSeXhiUFNMcERqbkZDTWpCd1pjdUNuNDFleWFoUDlGQmF1NHdScmVvMnJiT0N4R2NuVGVVZWhRNzRrVW5YWHpzLVZHbS16X1Zyal9yZU1uaEJQSDlULVllWE85VEFSSnozMWhwWTlEbHQ4aVNUczFpc1lwVF9SWjk5N1M4X3ZuZ3BRX09uNTBlS282MzNjSjNNSl9hRC13c3I1eVpvV2ZsY21ja1NLSUxkRWpXX1JUR25kdkYyYTd2dUNWMjhabTV2M2Rsd1Z1Q3BGeXV6Y2NSUWtWTEFYWktXV1JXU1Z3S21Eci02emxnam1peUVrR3RfSE5XN0dpTTZqVGxZZGVqaHN5RlhwdHZaVWYxcVg0U2w5ejZVYk82MjdhSnVyM21iX1htTVRGenRLZk9ZOWtHSk1rS0ZocEJ4b0NTZVQ2dVVfZ21IZ1FNUFRKOWt3VVVLRU5xZmxHRkVtWHd4UTJEMng1eFB6ZWFScmVtalV4SUV2WUhFV2ZFeDIwend1SDA2N0swWUNCWWdkdUoyWUlIc3FGSnNhQVNNeENxN1ZRMkhyblpOcnpoMERENElYX2xfUVlXZkpFeU1odE1Bek5oSTlOUXJvdkZ2NUR2YUU3M2s1Sm9QV2ZDQmdvNEtKV2Rsa2lLNmwxcUNwdVlWNktvcEQzV2JETENaUDk1OG9DV2FHOUc4c1l0NFRwcGhjX3lyb0F6U0xQcGlLWXNXR196T2E5ZWdTaFBNdS0xaGN6bHNYU01BOUNsZC15Sno3YnBNNktVdW42MndxRGZOTWtuOVBvbzFib0ppUURKWnhUWE5COUlTN0RpVnlqak5HQmhDRDgwWWRkZWlUME9veGRVYUxTX0F0UGhXeU1iVW01anA3OGVNYmdWX2ZPQVZfSjRMbGpGUW5VMEpvbzNybGcxZXpkdG1kZld1SFZNTFpyOVk2TV92WHpjeDBoWE9TaF93Wms4Y0xEZmF5T3NjWHZpNWdHMUxfLU52bXdYRnVyZEFCZHJFRm1WeUx2N3c3OV9xODczb0RkeDNsSndLWS43YzU2MnV2SDQ5LWRhRDRwcTQ5OXRRIiwidXNlcl90eXBlIjoidXNlciIsInRva2VuX2lkIjoiQWFaM3IwWWpJMVl6ZGhZakF0TnpjNU15MDBZelE1TFdKaU1EWXRPVE16T1RWalpUWmlZalExTURJelpEQmtOMlF0TURFeSIsInJlZmVyZW5jZV9pZCI6ImFjY2EyMTUyLThmYjAtNDBmYy05NjQzLTk1NDIyODk5YjEyZiIsImlzcyI6Imh0dHBzOlwvXC9pZGJyb2tlci1iLXVzLndlYmV4LmNvbVwvaWRiIiwidXNlcl9tb2RpZnlfdGltZXN0YW1wIjoiMjAyMzA2MDkxMDAyNDQuMzgyWiIsInJlYWxtIjoiYjY1N2ZjZjMtNmIwNi00NmE0LWFiMGUtZjIzZmNlYjdhNDQ3IiwiY2lzX3V1aWQiOiIwM2NmOWQzNC02ZGE5LTQxNTctYmRlZS1iODg0NzQ3Zjk1Y2IiLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiZXhwaXJ5X3RpbWUiOjE3Mjg0MzcyNTI3MjcsImNsaWVudF9pZCI6IkM2NGFiMDQ2MzllZWZlZTQ3OThmNThlN2JjM2ZlMDFkNDcxNjFiZTBkOTdmZjBkMzFlMDQwYTZmZmU2NmQ3ZjBhIn0.TPyBpG7IAhfCrU-QWzPJJi7dWQllvCFhpXFl8YAscuqnbHVdSs-L0AXgDR9WbtOVI1lq7NWS2suJGFIhYrEpAqa90pftx56T5ldK9cnL6TxAVmi9re-S3Eu-2cBAdUVa8GAj84LkLO_3DKKkuijqiRur0uY39ymt9L5Ljay_hAveKJF3MV_YX15b_LKGGCoFgaDBCi8DHUCQiFXQEezB6bNDrTJPH7yteBZvFdEh39mH2XsJ7fK9cJqkuDVCfysSq0aSD_PlyniU2Jf2rYhov5XPWqQjhDBfm3BQksTC7rfOTYBYTnUINOS1H-YRRlT9haleMHkZAKpmKOMyTHn6KQ";
+  //   access_token = "";
   // }
   // else {
   //   access_token = ""
